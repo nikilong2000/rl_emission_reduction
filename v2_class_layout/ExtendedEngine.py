d97aba8a3395c19d591be9152d9de8893d7c886d
nikilong2000 <niklaslongschiefelbein@gmail.com>
2026-01-22 13:52:06 +0100

    formatted v2; updated requirements for rl controller

--- v2_class_layout/ExtendedEngine.py
+++ v2_class_layout/ExtendedEngine.py
@@ -1,74 +1,76 @@
-import os # Operative system
-import numpy as np # Mathematics: arrays
+import os  # Operative system
+import numpy as np  # Mathematics: arrays
 import pandas as pd
 from keras.models import load_model
 import joblib
 
-class ExtendedEngine: #M0 ANN equivalent: engine + auxiliary controls that are now embedded inside M0
-    def __init__(self):
-         
-        return
-    
-    def load_model(self,str_dir_models,fileroot_model):
-        file_model=fileroot_model+'.h5'
-        self.model_filename=os.path.join(str_dir_models, file_model)
-        self.model=load_model(self.model_filename)
 
-        file_weights=fileroot_model+'_weights.hdf5'
-        self.weights_filename=os.path.join(str_dir_models, file_weights)
+class ExtendedEngine:  # M0 ANN equivalent: engine + auxiliary controls that are now embedded inside M0
+    def __init__(self):
+
+        return
+
+    def load_model(self, str_dir_models, fileroot_model):
+        file_model = fileroot_model + ".h5"
+        self.model_filename = os.path.join(str_dir_models, file_model)
+        self.model = load_model(self.model_filename)
+
+        file_weights = fileroot_model + "_weights.hdf5"
+        self.weights_filename = os.path.join(str_dir_models, file_weights)
         self.model.load_weights(self.weights_filename)
-        
+
         self.model.compile()
         print(self.model.summary())
-        
+
         return
 
-    def load_scalers(self,str_dir_scalers,fileroot_input,fileroot_output):
-        file_input=fileroot_input+'.lib'
-        self.scalerIn_filename=os.path.join(str_dir_scalers,file_input)
-        self.scalerIn=joblib.load(self.scalerIn_filename)
+    def load_scalers(self, str_dir_scalers, fileroot_input, fileroot_output):
+        file_input = fileroot_input + ".lib"
+        self.scalerIn_filename = os.path.join(str_dir_scalers, file_input)
+        self.scalerIn = joblib.load(self.scalerIn_filename)
 
-        file_output=fileroot_output+'.lib'
-        self.scalerOut_filename=os.path.join(str_dir_scalers,file_output)
-        self.scalerOut=joblib.load(self.scalerOut_filename)
+        file_output = fileroot_output + ".lib"
+        self.scalerOut_filename = os.path.join(str_dir_scalers, file_output)
+        self.scalerOut = joblib.load(self.scalerOut_filename)
         return
 
-    def create_main_in(self,Nobserv,Linput,Nvar):
-        self.main_in=np.ones((Nobserv,Linput,Nvar))
+    def create_main_in(self, Nobserv, Linput, Nvar):
+        self.main_in = np.ones((Nobserv, Linput, Nvar))
         return
 
-    def create_aux_ini(self,par1,par2,par3): #d_aux_ini to aux_ini, default/initial can be stored directly in the same variable
-        self.aux_ini=np.ones((par1,par2,par3))
-        self.aux_ini[0,0,0] = self.scalerOut.data_range_[0]/2
-        self.aux_ini[0,0,1] = self.scalerOut.data_range_[1]/2
-        self.aux_ini[0,0,2] = self.scalerOut.data_range_[0]/2
-        self.aux_ini[0,0,3] = self.scalerOut.data_range_[0]/2
+    def create_aux_ini(
+        self, par1, par2, par3
+    ):  # d_aux_ini to aux_ini, default/initial can be stored directly in the same variable
+        self.aux_ini = np.ones((par1, par2, par3))
+        self.aux_ini[0, 0, 0] = self.scalerOut.data_range_[0] / 2
+        self.aux_ini[0, 0, 1] = self.scalerOut.data_range_[1] / 2
+        self.aux_ini[0, 0, 2] = self.scalerOut.data_range_[0] / 2
+        self.aux_ini[0, 0, 3] = self.scalerOut.data_range_[0] / 2
         return
 
     def get_aux_ini(self):
-        return(self.scalerOut.transform(self.aux_ini[0]))
-            
-    def set_aux_ini(self,aux_ini):
-        self.aux_ini=aux_ini
+        return self.scalerOut.transform(self.aux_ini[0])
+
+    def set_aux_ini(self, aux_ini):
+        self.aux_ini = aux_ini
         return
-        
-    def set_main_in(self,v_k,n_ice_sp,P_EM1,P_EM2,GP,m_inj):
-        self.main_in[0,0,0] = v_k   # Velocity_kph
-        self.main_in[0,0,1] = n_ice_sp # Engine_Speed_RPM similar to set point
-        self.main_in[0,0,2] = P_EM1  # EM1 Power similar to set point
-        self.main_in[0,0,3] = P_EM2  # EM2 Power similar to set point
-        self.main_in[0,0,4] = GP   # Grade in percentage
-        self.main_in[0,0,5] = m_inj    # Injection mass similar to set point
+
+    def set_main_in(self, v_k, n_ice_sp, P_EM1, P_EM2, GP, m_inj):
+        self.main_in[0, 0, 0] = v_k  # Velocity_kph
+        self.main_in[0, 0, 1] = n_ice_sp  # Engine_Speed_RPM similar to set point
+        self.main_in[0, 0, 2] = P_EM1  # EM1 Power similar to set point
+        self.main_in[0, 0, 3] = P_EM2  # EM2 Power similar to set point
+        self.main_in[0, 0, 4] = GP  # Grade in percentage
+        self.main_in[0, 0, 5] = m_inj  # Injection mass similar to set point
 
         # Scaling input main
         self.main_in[0] = self.scalerIn.transform(self.main_in[0])
         return
 
     def run(self):
-        
-        v=self.model.predict([self.main_in,self.aux_ini],batch_size=1, steps=1)
-        vo=self.scalerOut.inverse_transform(v[0])
-        vi=self.scalerIn.inverse_transform(self.main_in[0])
 
-        return v,vi,vo   
-        
+        v = self.model.predict([self.main_in, self.aux_ini], batch_size=1, steps=1)
+        vo = self.scalerOut.inverse_transform(v[0])
+        vi = self.scalerIn.inverse_transform(self.main_in[0])
+
+        return v, vi, vo
--- v2_class_layout/LDCI_Layout.py
+++ v2_class_layout/LDCI_Layout.py
@@ -1,63 +1,63 @@
-#1.Loading libraries
-import numpy as np #Mathematics: arrays
-import matplotlib.pyplot as plt #Matplotlib
+# 1.Loading libraries
+import numpy as np  # Mathematics: arrays
+import matplotlib.pyplot as plt  # Matplotlib
 
 from PredictiveController import PredictiveController
 from ExtendedEngine import ExtendedEngine
 
+
 class LDCI_Layout:
     def __init__(self):
-        self.MyEngine=ExtendedEngine()
-        self.MyControl=PredictiveController()
+        self.MyEngine = ExtendedEngine()
+        self.MyControl = PredictiveController()
 
-        self.dataX, self.dataY, self.data_ctrl = [], [], [] #plot structures
+        self.dataX, self.dataY, self.data_ctrl = [], [], []  # plot structures
 
         return
 
-    def set_cycle(self,folder,file_cycle):
+    def set_cycle(self, folder, file_cycle):
         self.MyControl.load_cycle(folder, file_cycle)
         return
 
-    def set_basic_control(self,K_split,PCI,rend):
-        self.MyControl.set_gains(K_split,PCI,rend)
+    def set_basic_control(self, K_split, PCI, rend):
+        self.MyControl.set_gains(K_split, PCI, rend)
         return
 
+    def set_model(self, folder, fileroot_model, fileroot_scalerIn, fileroot_scalerOut):
 
-    def set_model(self,folder,fileroot_model,fileroot_scalerIn,fileroot_scalerOut):
+        # at this stage I consider these constants as local, later could be moved to main file
+        Nobserv = 1
+        Linput = 1
+        Nvar = 6
 
-        #at this stage I consider these constants as local, later could be moved to main file
-        Nobserv=1
-        Linput=1
-        Nvar=6
-
-        aux_par1=1
-        aux_par2=1
-        aux_par3=4
+        aux_par1 = 1
+        aux_par2 = 1
+        aux_par3 = 4
 
         self.MyEngine.load_model(folder, fileroot_model)
         self.MyEngine.load_scalers(folder, fileroot_scalerIn, fileroot_scalerOut)
-        self.MyEngine.create_main_in(Nobserv,Linput,Nvar)
-        self.MyEngine.create_aux_ini(aux_par1,aux_par2,aux_par3)
+        self.MyEngine.create_main_in(Nobserv, Linput, Nvar)
+        self.MyEngine.create_aux_ini(aux_par1, aux_par2, aux_par3)
 
         return
 
     def run(self):
 
-        aux_ini=self.MyEngine.get_aux_ini() #already covers the c==1 condition
-        Ncase=self.MyControl.get_Ncase()
-        vtime=self.MyControl.get_vect_time_cycle()
-        #Ncase=50 #for checks/debug
+        aux_ini = self.MyEngine.get_aux_ini()  # already covers the c==1 condition
+        Ncase = self.MyControl.get_Ncase()
+        vtime = self.MyControl.get_vect_time_cycle()
+        # Ncase=50 #for checks/debug
 
         for c in range(1, Ncase):
-            print('row %d: time=%.2f' % (c,vtime[c]))
-            #control
-            v_k,n_ice_sp,P_EM1,P_EM2,GP,m_inj=self.MyControl.control_loop(c)
+            print("row %d: time=%.2f" % (c, vtime[c]))
+            # control
+            v_k, n_ice_sp, P_EM1, P_EM2, GP, m_inj = self.MyControl.control_loop(c)
 
-            #engine
-            self.MyEngine.set_main_in(v_k,n_ice_sp,P_EM1,P_EM2,GP,m_inj)
-            v,vi,vo = self.MyEngine.run()
+            # engine
+            self.MyEngine.set_main_in(v_k, n_ice_sp, P_EM1, P_EM2, GP, m_inj)
+            v, vi, vo = self.MyEngine.run()
 
-            #plot_data+others
+            # plot_data+others
             self.dataX.append(vi)
             self.dataY.append(vo)
             self.data_ctrl.append(self.MyControl.get_plot_data())
@@ -65,58 +65,63 @@ class LDCI_Layout:
 
         return
 
-    def plot_dataset(self,column_names, dataset):
+    def plot_dataset(self, column_names, dataset):
         columns = {}
         for i, name in enumerate(column_names):
             columns[name] = dataset[:, i]
         # Creating subplots
         num_columns = len(columns)
-        fig, axes = plt.subplots(nrows=num_columns, ncols=1, figsize=(num_columns,10))
+        fig, axes = plt.subplots(nrows=num_columns, ncols=1, figsize=(num_columns, 10))
 
         # Plotting each column in a separate subplot
         for i in range(num_columns):
             axes[i].plot(columns[column_names[i]])
-            axes[i].set_xlabel('Index')
-            axes[i].set_ylabel('Value')
+            axes[i].set_xlabel("Index")
+            axes[i].set_ylabel("Value")
             axes[i].set_title(column_names[i])
 
         plt.tight_layout()
-        #plt.show() #I prefer to see the three figures together, moved to plot()
+        # plt.show() #I prefer to see the three figures together, moved to plot()
 
         return
 
     def plot(self):
 
-        #first plot: output variables
-    
+        # first plot: output variables
+
         n_rows = np.asarray(self.dataY).shape[0]
         n_cols = np.asarray(self.dataY).shape[2]
         Y = np.reshape(self.dataY, (n_rows, n_cols))
 
-        column_names = ['SOC', 'NO', 'NO2', 'Torque']
+        column_names = ["SOC", "NO", "NO2", "Torque"]
         self.plot_dataset(column_names, Y)
 
-        #second plot: input variables
+        # second plot: input variables
         n_rows2 = np.asarray(self.dataX).shape[0]
         n_cols2 = np.asarray(self.dataX).shape[2]
         X = np.reshape(self.dataX, (n_rows2, n_cols2))
 
-        column_names = ['v_k [km/h]', 'n_ice_sp [rpm]', 'P_EM1 [kW]', 'P_EM2 [kW]', 'Grade Profile [%]', 'm_inj [gps]']
+        column_names = [
+            "v_k [km/h]",
+            "n_ice_sp [rpm]",
+            "P_EM1 [kW]",
+            "P_EM2 [kW]",
+            "Grade Profile [%]",
+            "m_inj [gps]",
+        ]
         self.plot_dataset(column_names, X)
 
-        
-        #third plot: control variables
+        # third plot: control variables
         n_rows3 = np.asarray(self.data_ctrl).shape[0]
         n_cols3 = np.asarray(self.data_ctrl).shape[1]
         C = np.reshape(self.data_ctrl, (n_rows3, n_cols3))
 
-        column_names = ['P_req [kW]','ac_ped [%]','P_ice [kW]']
+        column_names = ["P_req [kW]", "ac_ped [%]", "P_ice [kW]"]
 
         self.plot_dataset(column_names, C)
-        
+
         plt.show()
 
-        #to be improved: position of figures to see all them at once
+        # to be improved: position of figures to see all them at once
 
         return
-        
